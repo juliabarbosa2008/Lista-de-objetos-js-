87da1ce878d9fa9e3a853d8f32ffcfaf4c8c1532
juliabarbosa2008 <julia.djubanski.barbosa@escola.pr.gov.br>
2025-09-25 11:50:21 +0000

    fim

---
+++ ativ.js/meuPrimeiroProjetoJava.js
@@ -0,0 +1,12 @@
+public class MeuPrimeiroProjetoJava {
+
+    public static void main(String[] args) {
+        System.out.println("Este é o meu primeiro programa em Java!");
+        
+        int a = 5;
+        int b = 3;
+        int soma = a + b;
+        
+        System.out.println("A soma de " + a + " + " + b + " é: " + soma);
+    }
+}
